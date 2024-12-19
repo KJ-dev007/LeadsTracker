f7ec619a46b716c2a8ae622270e59fd697b0b480
KJ-dev007 <kelvinnjoseph77@gmail.com>
2024-12-19 11:35:25 -0800

    developed HTML in Js

--- ChromeExt.js
+++ ChromeExt.js
@@ -1,7 +1,16 @@
-let myLeads = [];
+let myLeads = ['www.awesomelead.com', 'www.epiclead.com', 'www.greatlead.com'];
 const inputEl = document.querySelector('.input-el');
 const inputBtn = document.querySelector(".input-btn");
+const ulEl = document.querySelector('.ul-el');
+
 
 inputBtn.addEventListener('click', () => {
-  console.log('button clicked!');
-});
+  
+  myLeads.push(inputEl.value);
+  console.log(myLeads);
+})
+
+for (let i = 0; i < myLeads.length; i++){
+  ulEl.innerHTML += '<li>' + myLeads[i] + '</li>' ;
+}
+
