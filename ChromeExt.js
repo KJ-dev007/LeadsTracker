159137140b5dc0ce5ce18765cd8c74c706293171
KJ-dev007 <kelvinnjoseph77@gmail.com>
2024-12-19 15:25:07 -0800

    added a delete button and made it interactive

--- ChromeExt.js
+++ ChromeExt.js
@@ -2,13 +2,30 @@ let myLeads = [];
 const inputEl = document.querySelector('.input-el');
 const inputBtn = document.querySelector(".input-btn");
 const ulEl = document.querySelector('.ul-el');
-
-
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const deleteBtn = document.querySelector('.delete-btn');
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 if (leadsFromLocalStorage){
   myLeads= leadsFromLocalStorage;
-  renderLeads();
+  render(myLeads);
 }
+function render(leads){
+  let listItems ='';
+
+
+  for (let i = 0; i < leads.length; i++){
+    listItems += `<li> 
+      <a target='_blank' href= '${leads[i]}'>${leads[i]}</a> 
+      </li>` ;
+    
+  }
+
+  ulEl.innerHTML = listItems;
+}
+deleteBtn.addEventListener('dblclick', ()=>{
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+})
 
 
 inputBtn.addEventListener('click', () => {
@@ -16,21 +33,8 @@ inputBtn.addEventListener('click', () => {
   myLeads.push(inputEl.value);
   inputEl.value = '';
   localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  renderLeads();
+  render(myLeads);
   
   
 })
 
-function renderLeads(){
-    let listItems ='';
-
-
-    for (let i = 0; i < myLeads.length; i++){
-      listItems += `<li> 
-        <a target='_blank' href= '${myLeads[i]}'>${myLeads[i]}</a> 
-        </li>` ;
-      
-    }
-
-    ulEl.innerHTML = listItems;
-}
