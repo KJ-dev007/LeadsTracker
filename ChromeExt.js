365a8ec475d0d46931c9047dc338e93d5586d905
KJ-dev007 <kelvinnjoseph77@gmail.com>
2024-12-19 12:22:04 -0800

    introducing template strings on our anchor tags

--- ChromeExt.js
+++ ChromeExt.js
@@ -1,4 +1,4 @@
-let myLeads = ['www.awesomelead.com', 'www.epiclead.com', 'www.greatlead.com'];
+let myLeads = [];
 const inputEl = document.querySelector('.input-el');
 const inputBtn = document.querySelector(".input-btn");
 const ulEl = document.querySelector('.ul-el');
@@ -7,10 +7,21 @@ const ulEl = document.querySelector('.ul-el');
 inputBtn.addEventListener('click', () => {
   
   myLeads.push(inputEl.value);
-  console.log(myLeads);
+  inputEl.value = '';
+  renderLeads();
+  
 })
 
-for (let i = 0; i < myLeads.length; i++){
-  ulEl.innerHTML += '<li>' + myLeads[i] + '</li>' ;
-}
+function renderLeads(){
+    let listItems ='';
 
+
+    for (let i = 0; i < myLeads.length; i++){
+      listItems += `<li> 
+        <a target='_blank' href= '${myLeads[i]}'>${myLeads[i]}</a> 
+        </li>` ;
+      
+    }
+
+    ulEl.innerHTML = listItems;
+}
