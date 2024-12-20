2728eee3bb307ed6d43ea250a38627a541b9aca1
KJ-dev007 <kelvinnjoseph77@gmail.com>
2024-12-19 16:12:09 -0800

    Added the save tab button and made it interactive

--- ChromeExt.js
+++ ChromeExt.js
@@ -3,11 +3,24 @@ const inputEl = document.querySelector('.input-el');
 const inputBtn = document.querySelector(".input-btn");
 const ulEl = document.querySelector('.ul-el');
 const deleteBtn = document.querySelector('.delete-btn');
+const tabBtn = document.querySelector('.tab-btn');
+console.log(tabBtn);
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 if (leadsFromLocalStorage){
   myLeads= leadsFromLocalStorage;
   render(myLeads);
 }
+
+tabBtn.addEventListener('click', () =>{
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+  });
+  
+})
+
+
 function render(leads){
   let listItems ='';
 
@@ -37,4 +50,3 @@ inputBtn.addEventListener('click', () => {
   
   
 })
-
